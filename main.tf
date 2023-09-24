7c7b9bedf1f3f7c0fcb6dc2a1bf6b1a2aa6c15eb
Michał Ogrodnik <michal.ogrodnik@tesco.com>
2023-09-24 16:12:52 +0200

    fix validation

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "azure_naming" {
   source = "Azure/naming/azurerm"
   version = "0.3.0"
-  prefix                 = var.prefix
-  suffix                 = var.suffix
+  prefix                 = var.prefixes
+  suffix                 = var.suffixes
   unique-seed            = var.unique-seed
   unique-length          = var.unique-length
   unique-include-numbers = var.unique-include-numbers
--- variables.tf
+++ variables.tf
@@ -1,18 +1,22 @@
-variable "prefix" {
+variable "prefixes" {
   type        = list(string)
   default     = []
     validation {
-    condition     = ([] || [can(regex("^[0-9a-z]+$", var.prefix))])
+    condition     = alltrue([
+      for prefix in var.prefixes : can(regex("^[0-9a-z]*$", prefix))
+    ])
     error_message = "Prefix can contain only digits and lower case letters"
   }
   description = "It is not recommended that you use prefix by azure you should be using a suffix for your resources."
 }
 
-variable "suffix" {
+variable "suffixes" {
   type        = list(string)
   default     = []
-  validation {
-    condition     = ([] || [can(regex("^[0-9a-z]+$", var.suffix))])
+    validation {
+    condition     = alltrue([
+      for suffix in var.suffixes : can(regex("^[0-9a-z]+$", suffix))
+    ])
     error_message = "Suffix can contain only digits and lower case letters"
   }
   description = "It is recommended that you specify a suffix for consistency. please use only lowercase characters when possible"
@@ -22,7 +26,7 @@ variable "unique-seed" {
   description = "Custom value for the random characters to be used"
   type        = string
     validation {
-    condition     = ( "" || can(regex("^[0-9a-z]+$", var.unique-seed)))
+    condition     = can(regex("^[0-9a-z]*$", var.unique-seed))
     error_message = "Unique seed can contain only digits and lower case letters"
   }
   default     = ""
